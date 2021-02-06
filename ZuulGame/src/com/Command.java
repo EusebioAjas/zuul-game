9ffbdba5816a4b72528d6ecffeb9ffa34ed36df0
HikingCarrot7 <ricardoibarra2044@gmail.com>
2021-02-06 13:11:05 -0600

    feat(Command): added Command class code

--- ZuulGame/src/com/Command.java
+++ ZuulGame/src/com/Command.java
@@ -6,4 +6,28 @@ package com;
  */
 public class Command {
 
+    private final String commandWord;
+    private final String directionWord;
+
+    public Command(String commandWord, String directionWord) {
+	this.commandWord = commandWord;
+	this.directionWord = directionWord;
+    }
+
+    public String getCommandWord() {
+	return commandWord;
+    }
+
+    public String getDirectionWord() {
+	return directionWord;
+    }
+
+    public boolean isUnknown() {
+	return commandWord == null;
+    }
+
+    public boolean hasDirectionWord() {
+	return directionWord != null;
+    }
+
 }

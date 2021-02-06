2cd192b30e95dacc68760e44d98765506f7a2756
ajas-eusebio <A14008168@alumnos.uady.mx>
2021-02-06 13:53:02 -0600

    feat(CommandWords): add isCommand code

--- ZuulGame/src/com/CommandWords.java
+++ ZuulGame/src/com/CommandWords.java
@@ -1,10 +1,22 @@
 package com;
 
 public class CommandWords {
+
   private static final String[] validCommands = {
     "go", "quit", "help"
   };
 
   public CommandWords() {
+  }
+
+  public boolean isCommand(String str) {
+    boolean flag = false;
+    for (int i = 0; i < validCommands.length; i++) {
+      if (validCommands[i].equals(str)) {
+        flag = true;
+      }
+    }
+
+    return flag;
   }
 }

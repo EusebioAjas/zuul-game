662b5e1647575bca4b89e5b25088431e9db8af7d
alexisRosaldo <A14000657@alumnos.uady.mx>
2021-02-06 19:05:49 -0600

    feat(Room): modified setExits code

--- ZuulGame/src/com/Room.java
+++ ZuulGame/src/com/Room.java
@@ -20,6 +20,38 @@ public class Room {
     {
         this.description = description;
     }
+    
+      public void setExits(Room north, Room east, Room south, Room west) 
+    {
+        boolean flag1 = false, flag2 = false, flag3 = false, flag4 = false; 
+        if(north != null)
+            flag1 = true;
+           
+        if(east!=null)
+            flag2 = true;
+           
+        if(south != null)
+            flag3 = true;
+            
+        if(west!= null)
+            flag4 = true;
+        
+        if(flag1 ==true){
+            northExit = north;
+        }
+        if(flag2==true){
+            eastExit = east;
+        }
+        if(flag3 ==true)
+        {
+            southExit = south;
+        }
+        if(flag4 ==true)
+        {
+            westExit = west;
+         }
+        
+    }
  
     public String getDescription()
     {

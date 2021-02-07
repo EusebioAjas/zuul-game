63f6e8ca6f2dca775a0366a8df5c610f40c5f4b6
HikingCarrot7 <ricardoibarra2044@gmail.com>
2021-02-06 19:43:57 -0600

    feat(Parser) added parser class

--- ZuulGame/src/com/CommandWords.java
+++ ZuulGame/src/com/CommandWords.java
@@ -2,21 +2,19 @@ package com;
 
 public class CommandWords {
 
-  private static final String[] validCommands = {
-    "go", "quit", "help"
-  };
+    private static final String[] validCommands = {
+	"go", "quit", "help"
+    };
 
-  public CommandWords() {
-  }
-
-  public boolean isCommand(String str) {
-    boolean flag = false;
-    for (int i = 0; i < validCommands.length; i++) {
-      if (validCommands[i].equals(str)) {
-        flag = true;
-      }
+    public CommandWords() {
     }
 
-    return flag;
-  }
+    public boolean isCommand(String str) {
+	boolean flag = false;
+	for (int i = 0; i < validCommands.length; i++)
+	    if (validCommands[i].equals(str))
+		flag = true;
+
+	return flag;
+    }
 }

8f1162d89e2ecb57028348c19fb9be601961f513
ajas-eusebio <A14008168@alumnos.uady.mx>
2021-02-07 15:50:58 -0600

    feat(Game): added printHelp method code

--- ZuulGame/src/com/Game.java
+++ ZuulGame/src/com/Game.java
@@ -12,7 +12,11 @@ public class Game {
     parser = new Parser();
   }
   
-  public void printHelp(){
-    
+  private void printHelp(){
+    System.out.println("You are lost. YOu are alone. You wander");
+    System.out.println("around at the university.");
+    System.out.println();
+    System.out.println("Your command words are:");
+    System.out.println("   go quit help");
   }
 }
